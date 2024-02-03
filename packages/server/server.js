1d70e771c0d2f6c1aeadf0a52c6515399c2e29a4
Rashed Mahmud <rashed.webdev@gmail.com>
2024-02-03 22:39:52 +0600

    wip: initial project setup

--- packages/server/server.js
+++ packages/server/server.js
@@ -1,13 +1,40 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const connectDB = require('./config/dbConn');
+const morgan = require('morgan');
+const credentials = require('./middleware/credentials');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const errorHandler = require('./middleware/errorHandler');
+const mongoose = require('mongoose');
 
+// Connect to the database
+connectDB();
+
+// request logging
+app.use(morgan('dev'));
+
+// Middleware
+app.use(credentials);
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+app.use('/', express.static(path.join(__dirname, '/public')));
 
 app.get('/api/health', (req, res) => {
   res.send('OK');
 });
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+app.use(errorHandler);
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB');
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
 });
