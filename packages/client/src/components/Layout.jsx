b67562bd76438a54800478707e075248980648fa
Rashed Mahmud <rashed.webdev@gmail.com>
2024-02-12 07:59:27 +0600

    wip: ui and routing setup

--- packages/client/src/components/Layout.jsx
+++ packages/client/src/components/Layout.jsx
@@ -1,23 +1,45 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 import { Layout, Menu } from "antd";
-import {UserAddOutlined, UserOutlined, DashboardOutlined } from "@ant-design/icons";
+import {
+  UserAddOutlined,
+  UserOutlined,
+  DashboardOutlined
+} from "@ant-design/icons";
 
 const { Header } = Layout;
 
 function LayoutComponent() {
-
   const menuItems = [
-    {key: "1", icon: <UserAddOutlined />, label: <Link to="/signup">Sign up</Link>},
-    {key: "2", icon: <UserOutlined />, label: <Link to="/login">Login</Link>},
-    {key: "3", icon: <DashboardOutlined />, label: <Link to="/dashboard">Dashboard</Link>}
-  ]
+    {
+      key: "/signup",
+      icon: <UserAddOutlined />,
+      label: <Link to="/signup">Sign up</Link>
+    },
+    {
+      key: "/login",
+      icon: <UserOutlined />,
+      label: <Link to="/login">Login</Link>
+    }
+  ];
+
+  const location = useLocation();
+
   return (
     <Layout>
-      <Header>
+      <Header className="flex items-center">
+        <div className="w-[120px]">
+          <h3 className="text-white">
+            <Link to="/">Simple Note</Link>
+          </h3>
+        </div>
         <Menu
           theme="dark"
           mode="horizontal"
           items={menuItems}
+          selectedKeys={
+            location.pathname === "/" ? [] : [location.pathname]
+          }
+          className="grow"
         />
       </Header>
       <Outlet />
--- packages/client/src/main.jsx
+++ packages/client/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "antd/dist/reset.css";
+import "./css/index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider";
 
