1b8af5f0115473eaf7fcfeb3f9643edcf32b0ca1
Rashed Mahmud <rashed.webdev@gmail.com>
2024-03-10 07:49:38 +0600

    user controllers added

---
+++ packages/server/controllers/userController.js
@@ -0,0 +1,76 @@
+const bcrypt = require("bcrypt");
+const User = require("../model/User");
+
+const getAllUsers = async (_, res) => {
+  const users = await User.find().lean();
+  res.status(200).json({ message: "success", data: users });
+};
+
+const createUser = async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  if (!email || !password || !role)
+    return res
+      .status(400)
+      .json({ message: "Email, password and role are required" });
+
+  const duplicate = await User.findOne({ email }).exec();
+  if (duplicate)
+    return res.status(409).json({ message: "User already exists" });
+  const encryptedPassword = await bcrypt.hash(password, 10);
+  const createdUser = await User.create({
+    name,
+    email,
+    password: encryptedPassword,
+    role
+  });
+
+  const responseData = {
+    name: createdUser.name,
+    email: createdUser.email,
+    role: createdUser.role
+  };
+  return res.status(201).json({ message: "Success", data: responseData });
+};
+
+const getUserById = async (req, res) => {
+  const userId = req.params.id;
+  const user = await User.findById(userId, ["name", "email", "role"]).lean();
+  if (!user) return res.status(404).json({ message: "User not found" });
+
+  res.status(200).json({ message: "success", data: user });
+};
+
+const updateUser = async (req, res) => {
+  const userId = req.params.id;
+  const { name, email, role } = req.body;
+
+  const updatedUser = await User.findByIdAndUpdate(
+    userId,
+    { name, email, role },
+    { new: true }
+  ).lean();
+
+  const responseData = {
+    name: updatedUser.name,
+    email: updatedUser.email,
+    role: updatedUser.role
+  };
+
+  res.status(200).json({ message: "success", data: responseData });
+};
+
+const deleteUser = async (req, res) => {
+  const userId = req.params.id;
+  const deletedUser = await User.findByIdAndDelete(userId);
+  if (!deletedUser) return res.status(404).json({ message: "User not found" });
+  res.status(204).end();
+};
+
+module.exports = {
+  getAllUsers,
+  createUser,
+  getUserById,
+  updateUser,
+  deleteUser
+};
--- packages/server/routes/index.js
+++ packages/server/routes/index.js
@@ -4,6 +4,7 @@ const authController = require('../controllers/authController');
 const refreshTokenController = require('../controllers/refreshTokenController');
 const logoutController = require('../controllers/logoutController');
 const noteController = require('../controllers/noteController');
+const userController = require('../controllers/userController');
 const verifyJWT = require('../middleware/verifyJWT');
 const verifyRole = require('../middleware/verifyRole');
 const verifyMongooseId = require('../middleware/verifyMongooseId');
@@ -22,5 +23,14 @@ router.route('/api/notes/:id')
 .get(verifyRole, verifyMongooseId, noteController.getNoteById)
 .patch(verifyRole, verifyMongooseId, noteController.updateNote)
 .delete(verifyRole, verifyMongooseId, noteController.deleteNote);
+
+router.route('/api/users')
+.get(verifyRole, userController.getAllUsers)
+.post(verifyRole, userController.createUser);
+
+router.route('/api/users/:id')
+.get(verifyRole, verifyMongooseId, userController.getUserById)
+.patch(verifyRole, verifyMongooseId, userController.updateUser)
+.delete(verifyRole, verifyMongooseId, userController.deleteUser);
 
 module.exports = router;
